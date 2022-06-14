d58b9a23e19881666c9932168203662b45ed99af
速度七十迈的蜗牛 <lb18790140172@163.com>
2022-06-14 23:24:31 +0800

    fixed

---
+++ src/components/play/ControlBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="control-bar">
+    <i class="el-icon-share"></i>
+    <i class="el-icon-arrow-left"></i>
+    <i class="el-icon-video-play"></i>
+    <i class="el-icon-arrow-right"></i>
+    <i class="el-icon-star-off"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
