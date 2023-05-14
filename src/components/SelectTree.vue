e8cae220a6adf2c942f1a708a5d13f69a8abf190
速度七十迈的蜗牛 <lb18790140172@163.com>
2023-05-14 22:25:44 +0800

    fix(): 添加获取select tree 实例的方法

--- src/components/SelectTree.vue
+++ src/components/SelectTree.vue
@@ -231,6 +231,12 @@ export default {
     handleFocus() {
       this.filterMethod('');
     },
+    getSelect() {
+      return this.$refs['select'];
+    },
+    getTree() {
+      return this.$refs['tree'];
+    },
   },
 };
 </script>
