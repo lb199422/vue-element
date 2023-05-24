2767fa1508a878dbf42e431c9a9058873d4fadd2
速度七十迈的蜗牛 <lb18790140172@163.com>
2023-05-25 04:02:12 +0800

    fix(): 组件计划

--- src/components/DateQuarter.vue
+++ src/components/DateQuarter.vue
@@ -1,12 +1,24 @@
 <template>
-  <!--  el-date 扩展季度选择器 -->
-  <div></div>
+  <!--  基于el-select 扩展季度选择器 -->
+  <div>
+    <el-select v-model="date" placeholder="">
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      >
+      </el-option>
+    </el-select>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      date: '',
+    };
   },
 };
 </script>
---
+++ src/components/SelectPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- el-select 扩展  下拉数据据较多时进行分页展示  支持远程查询 -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
