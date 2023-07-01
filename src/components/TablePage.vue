a6cb6fa863fae937e3bda9587e240888261d210c
速度七十迈的蜗牛 <lb18790140172@163.com>
2023-07-01 23:48:12 +0800

    fix(): 分页

---
+++ src/components/TablePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- 带有搜索条件的表格分页组件 -->
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
