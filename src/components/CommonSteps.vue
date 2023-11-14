cb422d18f8570bc71ff6d6c62e25b379353466c0
lb199422 <lb18790140172@163.com>
2023-11-15 00:47:07 +0800

    fix(): 步骤条

---
+++ src/components/CommonSteps.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- ToDO  公共步骤条 -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
